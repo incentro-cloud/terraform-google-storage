b59b4c64f7d24cba15f7b4657ea1dfd7cb5dd795
Robin Smorenburg <robin@smorenburg.io>
2021-11-24 15:09:12 +0100

    Fix test

--- test/bucket_test.go
+++ test/bucket_test.go
@@ -11,7 +11,7 @@ func TestTerraformBucketExample(t *testing.T) {
 	projectId := os.Getenv("PROJECT_ID")
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/bucket_objects_iam_members",
+		TerraformDir: "../examples/bucket",
 		Vars: map[string]interface{}{
 			"project_id": projectId,
 		},
